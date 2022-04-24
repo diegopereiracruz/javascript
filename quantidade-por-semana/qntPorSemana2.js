46f84bce5594f9d0e8ed8e9b3e4093aa743247d9
Diego <66350572+diegopereiracruz@users.noreply.github.com>
2022-04-24 02:15:41 -0400

    Update qntPorSemana2.js

--- quantidade-por-semana/qntPorSemana2.js
+++ quantidade-por-semana/qntPorSemana2.js
@@ -3,22 +3,49 @@ var numSemana = 1;
 var numDia = 1;
 var semana = [];
 var totalDeSemanas = {};
+var operador = undefined;
+var printAlert;
 
-while (input != "q") {
-    input = prompt("Escreva um número de 0-9\nr = aleatório\nq = finalizar\n\n" + "[Semana " + numSemana + "]\nDia " + numDia);
-    if (input == Number(input)) {
-        if (numDia < 7) {
-            numDia++;
-            semana.push(input);
-        } else {
-            semana.push(input);
-            totalDeSemanas["semana" + numSemana] = semana;
-            numDia = 1;
-            numSemana++;
-            semana = [];
-        }
+while (operador != "s") {
+    operador = prompt("Quantidade por Semana\n[c] Criar\n[e] Exibir\n[m] Modificar\n[s] Sair\n\nFeito por: Diego Pereira Cruz");
+    switch (operador) {
+        case "c":
+            while (input != "q") {
+                input = prompt("Insira o valor:\nr = aleatório\nq = finalizar\n\n" + "[Semana " + numSemana + "]\nDia " + numDia);
+                if (input == Number(input)) {
+                    if (numDia < 7) {
+                        numDia++;
+                        semana.push(input);
+                    } else {
+                        semana.push(input);
+                        totalDeSemanas["semana" + numSemana] = semana;
+                        numDia = 1;
+                        numSemana++;
+                        semana = [];
+                    }
+                }
+            }
+            break;
+        
+        case "e":
+            var keys = Object.keys(totalDeSemanas);
+            keys.forEach((keys, index) => {
+                printAlert = `${keys}: ${totalDeSemanas[keys]}\n`;
+            });
+            break;
+        
+        case "s":
+            operador = "s";
+            break;
+        
+        default:
+            alert("Ação inválida!");
+            break;
     }
 }
+operador = undefined;
+
+
 
 var keys = Object.keys(totalDeSemanas);
 keys.forEach((keys, index) => {

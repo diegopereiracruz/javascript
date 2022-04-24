054ee5f521f3b4761b877ed1974fbc475c7f80f2
Diego <66350572+diegopereiracruz@users.noreply.github.com>
2022-04-23 20:50:22 -0400

    Update qntPorSemana.js

--- quantidade-por-semana/qntPorSemana.js
+++ quantidade-por-semana/qntPorSemana.js
@@ -1,7 +1,7 @@
 var qntPerDay = [7,2,4,9,0,2,7,0,7,5,2,1,9,0]; // 14 days; 2 weeks
 var week = 2; // Semana escolhida
 var daysWeek = 7; // Dias por semana
-var numWeek = birdsPerDay.length / daysWeek; // Qnt. de semanas
+var numWeek = qntPerDay.length / daysWeek; // Qnt. de semanas
 var total = 0; // Total por semana
 
 var arrayWeeks = [];
@@ -10,14 +10,14 @@ var temp = []; // Array temporário
 // PROCESSAMENTO
 
 for (var j = 0; j < numWeek; j++) {
-    arrayWeeks[j] = 1 + j; // ATENÇÃO! 0 = 1
+    arrayWeeks[j] = 1 + j; // ATENÇÃO! array[0] = 1
     //console.log(arrayWeeks[j]);
 }
 
-if (week == arrayWeeks[week - 1]) { // ATENÇÃO! 2 = 1
-    if 
+if (week == arrayWeeks[week - 1]) { // ATENÇÃO! 1 == array[0]
     
-} else console.log("Erro: if (week == arrayWeeks)");
+    
+} else console.log("Erro: a semana escolhida não coincide.");
 
 /*
 

60dec6dfe173181328858cd97fa0451f6f94148e
Diego <66350572+diegopereiracruz@users.noreply.github.com>
2022-04-27 14:36:33 -0400

    Update collatzConjecture.js

--- collatz_conjecture/collatzConjecture.js
+++ collatz_conjecture/collatzConjecture.js
@@ -1,19 +1,20 @@
 var x = undefined;
+var repetitions = 0;
 
 while (x != "q") {
-    x = prompt("A Conjectura de Collatz\nwiki: O que é?\nq: Sair\n\nDê o valor inicial");
+    x = prompt("Conjectura de Collatz\na: O que é?\nq: Sair\n\nDigite um número natural inteiro (ex.: 7):");
     
     switch (x){
-        case "wiki":
-            window.open("http://pt.wikipedia.org/wiki/Conjectura_de_Collatz", "_blank");
+        case "a":
+            y = prompt("A Conjectura de Collatz ou também é conhecido como Problema 3x + 1, é um problema matemático proposto pelo matemático alemão Lothar Collatz, em 1937.\n\nO problema consiste em obter qualquer número natural inteiro, caso o mesmo seja par, será dividido por 2 (x / 2), caso seja impar, será multiplicado por 3 e adicionado 1 (x * 3 + 1).\nEntretanto, qualquer número natural inteiro, eventualmente chegará a 4, que se converte em 2 e terminando em 1, entrando em uma sequência de 4, 2, 1 infinitamente.\n\nwiki: ver na wikipédia.\nqualquer tecla: voltar");
+            if (y == "wiki" || y == "Wiki" || y == "WIKI") {
+                window.open("http://pt.wikipedia.org/wiki/Conjectura_de_Collatz", "_blank");
+            } else {
+                x = undefined;
+                y = undefined;
+                break;
+            }
             
-            x = undefined;
-            break;
-            
-        case x == Number(x):
-            
-            
-            x = undefined;
             break;
         
         case "q":
@@ -21,11 +22,17 @@ while (x != "q") {
             break;
         
         default:
-            if (x == Number()) {
+            if (!Number.isNaN(x)) {
                 while (x) {
-                    if (x == 4) {
-                        alert("Caiu no loop");
-                        break;
+                    if (x == 1) {
+                        repetitions++;
+                        alert(x = x * 3 + 1);
+                        
+                        if (repetitions == 2) {
+                            alert(x / 2);
+                            alert("Caiu no loop");
+                            break;
+                        }
                     }
                     else if (x % 2 == 1) {
                         alert(x = x * 3 + 1);
@@ -33,13 +40,11 @@ while (x != "q") {
                         alert(x = x / 2);
                     }
                 }
-            } else {
-                alert("Apenas número ou wiki!");
-                x - undefined;
+            } else if (Number.inNaN(x)) {
+                alert("Entrada inválida!\nInsira um número ou um comando.");
+                x = undefined;
             }
             
             break;
     }
-    
 }
-

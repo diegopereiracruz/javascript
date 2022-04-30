f89ed00f8bd6bc30d32d62680134f94d078f3182
Diego <66350572+diegopereiracruz@users.noreply.github.com>
2022-04-29 21:50:50 -0400

    Create collatzConjecture_v2.js

---
+++ collatz_conjecture/collatzConjecture_v2.js
@@ -0,0 +1,50 @@
+var x = undefined;
+var y = undefined;
+//var vezesQueUmRepetiu = 0;
+var arrayDeValores = [];
+
+while (x != "q") {
+    x = prompt("Conjectura de Collatz\na: O que é?\nq: Sair\n\nDigite um número natural inteiro:");
+    
+    switch (x){
+        case "a":
+            y = prompt("A Conjectura de Collatz ou também é conhecido como Problema 3x + 1, é um problema matemático proposto pelo matemático alemão Lothar Collatz, em 1937.\n\nO problema consiste em obter qualquer número natural inteiro, caso o mesmo seja par, será dividido por 2 (x / 2), caso seja impar, será multiplicado por 3 e adicionado 1 (x * 3 + 1).\nEntretanto, qualquer número natural inteiro, eventualmente chegará a 4, que se converte em 2 e terminando em 1, entrando em uma sequência de 4, 2, 1 infinitamente.\n\nwiki: ver na wikipédia.\nqualquer tecla: voltar");
+            if (y == "wiki" || y == "Wiki" || y == "WIKI") {
+                window.open("http://pt.wikipedia.org/wiki/Conjectura_de_Collatz", "_blank");
+            } else {
+                x = undefined;
+                y = undefined;
+                break;
+            }
+        
+        case "q":
+            break;
+        
+        default:
+            arrayDeValores.push(Number(x));
+            
+            while (arrayDeValores.length != 0) {
+                if (arrayDeValores.slice(arrayDeValores.length - 1) == 1) {
+                    x = prompt("Total de sequências: " + arrayDeValores.length + "\nTodos os valores:\n" + arrayDeValores + "\n\n1 * 3 + 1 = 4 | 4 / 2 = 2 | 2 / 2 = 1...\n\nq: voltar");
+                    arrayDeValores = [];
+                    break;
+                    
+                } else if (arrayDeValores.slice(arrayDeValores.length - 1) % 2 == 1) {
+                    alert(arrayDeValores.slice(arrayDeValores.length - 1) + "\n\nImpar (" + arrayDeValores.slice(arrayDeValores.length - 1) + " * 3 + 1)\n" + arrayDeValores);
+                    arrayDeValores.push(arrayDeValores.slice(arrayDeValores.length - 1) * 3 + 1);
+                    //vezesQueUmRepetiu = (arrayDeValores.slice(arrayDeValores.length - 1) == 1) ? vezesQueUmRepetiu++;
+                    
+                } else if (arrayDeValores.slice(arrayDeValores.length - 1) % 2 == 0) {
+                    alert(arrayDeValores.slice(arrayDeValores.length - 1) + "\n\nPar (" + arrayDeValores.slice(arrayDeValores.length - 1) + " / 2)\n" + arrayDeValores);
+                    arrayDeValores.push(arrayDeValores.slice(arrayDeValores.length - 1) / 2);
+                    
+                } else if (Number.isNaN(x)) {
+                    alert("Entrada inválida!\nInsira um número ou um comando.");
+                    x = undefined;
+                    break;
+                }
+            }
+            
+            break;
+    }
+}

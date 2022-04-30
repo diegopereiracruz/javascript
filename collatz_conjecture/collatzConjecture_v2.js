64a4d22b98a63b09957dd3a959b23386e21f83c8
Diego <66350572+diegopereiracruz@users.noreply.github.com>
2022-04-29 22:11:06 -0400

    Update collatzConjecture_v2.js

--- collatz_conjecture/collatzConjecture_v2.js
+++ collatz_conjecture/collatzConjecture_v2.js
@@ -3,11 +3,11 @@ var y = undefined;
 var arrayDeValores = [];
 
 while (x != "q") {
-    x = prompt("Conjectura de Collatz\na: O que é?\nq: Sair\n\nDigite um número natural inteiro:");
+    x = prompt("Conjectura de Collatz\na: o que é?\nq: sair\n\nDigite um número natural inteiro:");
     
     switch (x){
         case "a":
-            y = prompt("A Conjectura de Collatz ou também é conhecido como Problema 3x + 1, é um problema matemático proposto pelo matemático alemão Lothar Collatz, em 1937.\n\nO problema consiste em obter qualquer número natural inteiro, caso o mesmo seja par, será dividido por 2 (x / 2), caso seja impar, será multiplicado por 3 e adicionado 1 (x * 3 + 1).\nEntretanto, qualquer número natural inteiro, eventualmente chegará a 4, que se converte em 2 e terminando em 1, entrando em uma sequência de 4, 2, 1 infinitamente.\n\nwiki: ver na wikipédia.\nqualquer tecla: voltar");
+            y = prompt("A Conjectura de Collatz ou também é conhecido como Problema 3x + 1, é um problema matemático proposto pelo matemático alemão Lothar Collatz, em 1937.\n\nO problema consiste em obter qualquer número natural inteiro, caso o mesmo seja par, será dividido por 2 (x / 2), caso seja impar, será multiplicado por 3 e adicionado 1 (x * 3 + 1).\n\nEntretanto, qualquer número natural inteiro, eventualmente chegará a 4, que se converte em 2 e terminando em 1, entrando em uma sequência de 4, 2, 1 infinitamente.\n\nwiki: ver na Wikipédia\nqualquer tecla: voltar");
             if (y == "wiki" || y == "Wiki" || y == "WIKI") {
                 window.open("http://pt.wikipedia.org/wiki/Conjectura_de_Collatz", "_blank");
             } else {
@@ -24,7 +24,7 @@ while (x != "q") {
             
             while (arrayDeValores.length != 0) {
                 if (arrayDeValores.slice(arrayDeValores.length - 1) == 1) {
-                    x = prompt("Total de sequências: " + arrayDeValores.length + "\n\nTodos os valores:\n" + arrayDeValores.join(" | ") + "\n\n4, 2, 1 se repetirá infinitamente\n\nq: voltar");
+                    x = prompt("Total de sequências: " + arrayDeValores.length + "\n\nTodos os valores:\n" + arrayDeValores.join(" | ") + "\n\n4, 2, 1 se repetirá infinitamente\n\nqualquer tecla: voltar");
                     arrayDeValores = [];
                     x = undefined;
                     break;
